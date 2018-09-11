ea733e2b9dcaca78e741b6ebc1ae3c560c84c413
Regular Guy <Buiduk@gmail.com>
2018-09-11 17:43:47 +0700

    ok
    ok lan 2

--- backend/router/OrderRouter.js
+++ backend/router/OrderRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const orderRouter = express.Router();
-const orderModel = require("../model/ProductModel");
+const orderModel = require("../model/OrderModel");
 
 
 //Lấy thông tin sản phẩm
 orderRouter.get("/:id", (req, res) => {
     orderModel.findById(req.params.id)
+        .populate('owner', 'email name avatarUrl')
+        .populate('orderList.product')
         .then(orderFound => {
             if (!orderFound) res.status(404).send({ success: 0, message: 'Order Not Found' })
             else res.send({ success: 1, orderFound });
